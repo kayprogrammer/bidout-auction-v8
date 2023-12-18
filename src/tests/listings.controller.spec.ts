79014b186e3920f0546008c304f8ac09cf8f1c54
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-12-18 10:34:38 +0100

    tests: listing detail

--- src/tests/listings.controller.spec.ts
+++ src/tests/listings.controller.spec.ts
@@ -52,17 +52,30 @@ describe('ListingsController', () => {
 
     it('Should return listing detail', async () => {
 
-        // Create Listing
-        await listingService.testListing()
-
         // Test
-        const result = testGet(api, '/listings');
-        await result.expect(200)
+        // For invalid slug
+        let result = testGet(api, '/listings/detail/invalid_slug');
+        await result.expect(404)
+        await result.expect((response) => {
+          const respBody = response.body
+          expect(respBody).toHaveProperty('status', 'failure');
+          expect(respBody).toHaveProperty('message', 'Listing does not exist!');
+          expect(respBody.data).toHaveLength(1);
+        })
+
+        // For valid slug
+        // Get Listing
+        const listing = await listingService.testListing()
+        result = testGet(api, `/listings/detail/${listing.slug}`);
+        await result.expect(404)
         await result.expect((response) => {
           const respBody = response.body
           expect(respBody).toHaveProperty('status', 'success');
-          expect(respBody).toHaveProperty('message', 'Listings fetched');
-          expect(respBody.data).toHaveLength(1);
+          expect(respBody).toHaveProperty('message', 'Listing details fetched');
+          const expectedKeys: string[] = ["name", "auctioneer", "slug", "desc", "category", "price", "closing_date", "time_left_seconds", "active", "bids_count", "highest_bid", "image", "watchlist"];
+          for (const key of expectedKeys) {
+            expect(Object.keys(respBody.data)).toContain(key);
+          }
         })
     });
 
