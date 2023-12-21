de073dd91e477cd17136288b569ffa42c51dc994
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-12-21 14:08:07 +0100

    endpoints: create listings

--- src/controllers/auctioneer.controller.ts
+++ src/controllers/auctioneer.controller.ts
@@ -8,14 +8,19 @@ import { RequestError } from '../exceptions.filter';
 import { UserService } from '../../prisma/services/accounts.service';
 import { AuthService } from '../utils/auth.service';
 import { FileService } from '../../prisma/services/general.service';
+import { CreateListingResponseDataSchema, CreateListingResponseSchema, CreateListingSchema } from '../schemas/auctioneer';
+import { Category } from '@prisma/client';
 
 @Controller('api/v8/auctioneer')
 @ApiTags('Auctioneer')
+@UseGuards(AuthGuard)
+@ApiBearerAuth()
 export class AuctioneerController {
   constructor(
     private readonly userService: UserService,
     private readonly authService: AuthService,
     private readonly listingsService: ListingService,
+    private readonly categoryService: CategoryService,
     private readonly bidsService: BidService,
     private readonly fileService: FileService,
 
@@ -25,8 +30,6 @@ export class AuctioneerController {
   @ApiOperation({ summary: 'Retrieve all listings by the current user', description: "This endpoint retrieves all listings by the current user" })
   @ApiResponse({ status: 200, type: ListingsResponseSchema })
   @ApiQuery({ name: 'quantity', required: false, type: Number, description: 'Quantity to fetch' })
-  @ApiBearerAuth()
-  @UseGuards(AuthGuard)
   async retrieveAuctioneerListings(@Req() req: any, @Query("quantity") quantity?: number): Promise<ListingsResponseSchema> {
     const listings = await this.listingsService.getByAuctioneerId(req.user.id, quantity);
     // Return response
@@ -35,6 +38,27 @@ export class AuctioneerController {
       'Auctioneer Listings fetched', 
       listings, 
       ListingSchema
+    )
+  }
+
+  @Post("/listings")
+  @ApiOperation({ summary: 'Create a listing', description: "This endpoint creates a new listing. Note: Use the returned file_upload_data to upload image to cloudinary" })
+  @ApiResponse({ status: 201, type: CreateListingResponseSchema })
+  @ApiBearerAuth()
+  async createListing(@Req() req: any, @Param("slug") slug: string, @Body() data: CreateListingSchema): Promise<CreateListingResponseSchema> {
+    let categorySlug = data.category
+    let category: Promise<Category | null> = null
+
+    const fileType = data.fileType
+    if(categorySlug !== "other") {
+      category = await this.categoryService.getBySlug(categorySlug)
+    }
+    // Return response
+    return Response(
+      CreateListingResponseSchema, 
+      'Listing created successfully', 
+      listing, 
+      CreateListingResponseDataSchema
     )
   }
 }
