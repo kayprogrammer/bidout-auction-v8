137da1e5c072279d51f51755ae2a0edbda5e1d71
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-12-26 16:29:48 +0100

    tests: create listing

--- src/tests/auctioneer.controller.spec.ts
+++ src/tests/auctioneer.controller.spec.ts
@@ -4,10 +4,11 @@ import { UserService } from "../../prisma/services/accounts.service";
 import { PrismaService } from "../prisma.service";
 import { BidService, CategoryService, ListingService, WatchlistService } from "../../prisma/services/listings.service";
 import { AuthService } from "../utils/auth.service";
-import { authTestGet, authTestPost, testGet } from "./utils";
+import { authTestGet, authTestPost } from "./utils";
 import { FileService } from "../../prisma/services/general.service";
 import { AuctioneerModule } from "../modules/auctioneer.module";
-const listingsExpectedKeys: string[] = ["name", "auctioneer", "slug", "desc", "category", "price", "closing_date", "time_left_seconds", "active", "bids_count", "highest_bid", "image", "watchlist"];
+import { error } from "console";
+const listingsExpectedKeys: string[] = ["name", "auctioneer", "slug", "desc", "category", "price", "closing_date", "time_left_seconds", "active", "bids_count", "highest_bid", "file_upload_data"];
 
 describe('AuctioneerController', () => {
     let api: supertest.SuperTest<supertest.Test>;
@@ -47,6 +48,41 @@ describe('AuctioneerController', () => {
         expect(respBody).toHaveProperty("status", "success"); 
         expect(respBody).toHaveProperty('message', 'Auctioneer Listings fetched');
         expect(respBody.data).toHaveLength(1);
+    });
+
+    it('Should create a listing', async () => {
+        const category = await categoryService.testCategory()
+        const listingDict = {
+            name: "Test Listing",
+            desc: "Test description",
+            category: category.slug,
+            price: 1000.00,
+            closingDate: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+            fileType: "image/jpeg",
+        }
+    
+        // Test
+        const user = await userService.testVerifiedUser();
+        let result = await authTestPost(api, `/auctioneer/listings`, authService, userService, user, listingDict);
+        
+        // Assertions
+        expect(result.statusCode).toBe(201);
+        let respBody = result.body
+        expect(respBody).toHaveProperty('status', 'success');
+        expect(respBody).toHaveProperty('message', 'Listing created successfully');
+        for (const key of listingsExpectedKeys) {
+            expect(Object.keys(respBody.data)).toContain(key);
+        }
+
+        // Test if error is returned because of invalid category
+        listingDict.category = "invalid-category"
+        result = await authTestPost(api, `/auctioneer/listings`, authService, userService, user, listingDict);
+        
+        // Assertions
+        expect(result.statusCode).toBe(422);
+        respBody = result.body
+        expect(respBody).toHaveProperty('message', 'Invalid Entry');
+        expect(respBody.data).toHaveProperty('category', 'Invalid Category');
     });
 
     afterAll(async () => {
