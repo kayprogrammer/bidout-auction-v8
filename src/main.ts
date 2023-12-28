288754f68a70b6cb5b622dcde0a1b92fa7646096
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-12-28 01:06:57 +0100

    update: swagger config

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,19 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('', app, document, {
+      customSiteTitle: settings.projectName,
+      customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+      customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+      ],
+      customCssUrl: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+      ],
+  });
 
   await app.listen(8000);
   if (module.hot) {
